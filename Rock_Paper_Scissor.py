9d3c7951269305f6f1805d6169cdc732313a0bb5
saurabhgit2 <saurabhgithub0@gmail.com>
2026-03-06 01:02:49 +1300

    Added the remaining code of the game

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -2,8 +2,11 @@
 # importing random package
 import random
 
+user_wins = 0
+computer_wins = 0
+
 # options for game
-options=["Rock","Paper","Scissor"]
+options=["rock","paper","scissor"]
 
 while True:
     user_input=input("Type Rock/Paper/Scissor or Q to quit the game ").lower()
@@ -11,7 +14,27 @@ while True:
         break
     if user_input not in options:
         continue
+
     random_number=random.randint(0,2)
     # rock:0 paper:1 scissor:2
+    computer_pick=options[random_number]
+    print("Computer picked",computer_pick + ".")
+   
+    if user_input=="rock" and computer_pick=="scissor":
+        print("You won, ")
+        user_wins+=1
 
-print("GoodBye!")
+    elif user_input=="paper" and computer_pick=="rock":
+        print("You won, ")
+        user_wins+=1
+
+    elif user_input=="scissor" and computer_pick=="paper":
+        print("You won, ")
+        user_wins+=1
+    
+    else:
+        print("Computer Won")
+        computer_wins+=1
+
+
+print("Good Bye!")
